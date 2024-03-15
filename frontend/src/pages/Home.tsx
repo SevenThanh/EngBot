88bc7114be780e2b2fbe5a5df56eb940fb0e43e9
Kevin Cheng <kev72eat@gmail.com>
2024-03-14 22:07:01 -0400

    add main page footer

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,13 @@
 import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
+import Footer from '@/components/Footer'
 
 export default function Home() {
     return (
-        <div className="w-screen h-screen">
+        <div className="w-screen h-screen overflow-y-auto">
             <Navbar />
-            <main className="w-screen h-full">
-                <Hero />
-            </main>
+            <Hero />
+            <Footer />
         </div>
     )
 }
