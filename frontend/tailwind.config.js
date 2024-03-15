72eb851790f9a3ff84b08699759d7dea3e0ac124
Kevin Cheng <kev72eat@gmail.com>
2024-03-14 21:23:17 -0400

    make simple tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ['.src/**/*.{js,ts,jsx,tsx}'],
+  content: ["index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-    },
-    fontFamily: {
+      background: 'rgb(249 250 251)',
+      background_gray: 'rgb(24 24 27)',
+      primary: 'rgb(125 211 252)',
+      accent: 'rgb(37 99 235)',
     }
-  }
+  },
+  plugins: []
 }
