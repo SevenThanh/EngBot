7d95136bf1256475e00adee8e26d16c04ed6534e
Kevin Cheng <kev72eat@gmail.com>
2024-03-21 11:45:05 -0400

    layouts for other main page sections

---
+++ frontend/src/components/ExperienceSlide.tsx
@@ -0,0 +1,9 @@
+export default function ExperienceSlide() {
+    return (
+        <div className="w-full h-screen py-1/10 px-1/5">
+            <h1></h1>
+            <main className="">
+            </main>
+        </div>
+    )
+}
---
+++ frontend/src/components/LearnSlide.tsx
@@ -0,0 +1,16 @@
+export function LearnSlide() {
+    return (
+        <div className="w-full h-screen py-1/10 px-1/5">
+            <main className="flex flex-row h-full">
+                <h1 className="text-4xl text-accent">Learn at your level</h1>
+                <p className="text-xl">
+                    Whether you're a student, worker, or a parent looking to
+                    boost your English, we have everything you need.
+                </p>
+            </main>
+            <div className="w-full h-1/5 bg-gradient-to-r from-primary
+                            to-background flex flex-row gap-24">
+            </div>
+        </div>
+    )
+}
