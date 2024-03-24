80c69ebbaa408536c6e38b7c826666bc5578cc12
Kelly Chan <kelllycchan@gmail.com>
2024-03-24 19:09:03 -0400

    finished learning experiences section of the main page

--- frontend/src/components/ExperienceSlide.tsx
+++ frontend/src/components/ExperienceSlide.tsx
@@ -2,8 +2,15 @@ export default function ExperienceSlide() {
     return (
         <div className="w-full h-screen bg-background flex flex-col">
             <div className="w-full grow bg-background_gray"></div>
-            <h1 className="text-6xl font-bold"></h1>
-            <main className="">
+            <main className="flex flex-col items-center gap-4 px-1/10 mt-[10vh] mb-[30vh]">
+                <h1 className="text-6xl font-bold">Experience Personalized Learning</h1>
+                <div className="grid grid-cols-3 px-1/10 mt-[10vh]">
+                    <p className="col-span-2 text-2xl text-left">
+                        Scenarios ranging from classroom conversations to everyday tasks, all while buildling a strong foundation in English and grammar vocabulary.
+                    </p>
+                    <img src="" alt="image that is supposed to be there"/>
+                </div>
+                
             </main>
         </div>
     )
