718cb7d5ef8a3ea722e04721a3760a0c672919eb
Kevin Cheng <kev72eat@gmail.com>
2024-03-26 19:54:21 -0400

    display icons on the main page

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Icon from './Icon'
 
 export default function Navbar() {
     return (
-        <nav className="h-24 w-full bg-background top-0 p-6 flex flex-row justify-between">
+        <nav className="h-24 w-full bg-background p-6 flex flex-row justify-between">
             <Icon />
             <button className="px-8 rounded-lg border-2 border-light_300 shadow">
                 Log In
