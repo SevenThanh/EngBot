34e686e313be17139a070a6adbf0ccc7be75f77b
Kevin Cheng <kev72eat@gmail.com>
2024-03-28 22:14:23 -0400

    commit routes for signup/login pages

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,9 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from "@/pages/Home"
+import { Signup } from "@pages/Signup.tsx"
+import { Login } from "@pages/Login.tsx"
 
 export default function AppRoutes() {
     return (
         <Routes>
+            <Route path="signup" element={<Signup />} />
+            <Route path="login" element={<Login />} />
             <Route path="*" element={<Home />} />
         </Routes>
     )
