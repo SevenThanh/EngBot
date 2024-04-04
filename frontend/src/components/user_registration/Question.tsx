a43a6453bb5d6978f3e74de4cfb9cf49b9ded083
Kevin Cheng <kev72eat@gmail.com>
2024-04-04 00:20:23 -0400

    add user registration questions into UserRegistration

--- frontend/src/components/user_registration/Question.tsx
+++ frontend/src/components/user_registration/Question.tsx
@@ -1,13 +1,16 @@
 interface QuestionProps {
     question: string,
     answers: string[],
+    small?: string,
+    type?: string,
 }
 
-export function Question({ question, answers }: QuestionProps) {
+export function Question({ question, answers, small, type="single" }: QuestionProps) {
     //just style this for now :)
     return (
         <section className="flex flex-col">
             <h1>{question}</h1>
+            {small && <p>{small}</p>}
             {answers.map(ans => <div>{ans}</div>)}
         </section>
     )
--- frontend/src/pages/UserRegistration.tsx
+++ frontend/src/pages/UserRegistration.tsx
@@ -1,11 +1,61 @@
+import { useState } from 'react'
 import { Question } from '@/components/user_registration'
 import { ProgressBar } from '@/components/user_registration'
 
+const questions = [
+    {
+        question: "Which best describes you?",
+        answers: [
+            "Student",
+            "Teacher",
+            "Parent",
+            "Professional",
+            "Other"
+        ]
+    },
+    {
+        question: "Select your English proficiency.",
+        answers: [
+            "New",
+            "Beginner",
+            "Intermediate",
+            "Advanced"
+        ]
+    },
+    {
+        question: "Why do you want to learn English?",
+        small: "select all that apply",
+        type: "multiselect",
+        answers: [
+            "Education",
+            "Connect with others",
+            "Travel",
+            "For fun",
+            "Other"
+        ]
+    },
+    {
+        question: "How did you hear about us?",
+        answers: [
+            "TikTok",
+            "YouTube",
+            "Instagram",
+            "Google Search",
+            "Other"
+        ]
+    },
+]
+
 export function UserRegistration() {
+    const [questionData, setQuestionData] = useState(0);
+
     return (
-        <main className="gradient-background">
+        <div className="w-screen h-screen bg-engbot-gradient">
             <ProgressBar progress={0} />
-            <Question question="rahhh" answers={["raaaaah x2"]}/>
-        </main>
+            <main className="grid grid-cols-2">
+                <img src="main.png" alt="ENGBOT"/>
+                <Question {...questions[questionData]}/>
+            </main>
+        </div>
     )
 }
