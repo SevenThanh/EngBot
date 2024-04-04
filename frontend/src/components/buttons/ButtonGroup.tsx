2ddc30669e23ff17ff59197c1e9a1a53b26e178d
Kevin Cheng <kev72eat@gmail.com>
2024-04-04 00:54:19 -0400

    define types for button groups

--- frontend/src/components/buttons/ButtonGroup.tsx
+++ frontend/src/components/buttons/ButtonGroup.tsx
@@ -1,19 +1,36 @@
 import { createContext, useState } from "react"
 import { SelectionButton } from "./SelectionButton"
 
+type Label = {
+    label?: string,
+    text: string
+}[] | string[]
+
+export type ButtonGroupType = {
+    buttonState: number,
+    setButtonState: (buttonState: number) => void
+}
+
 interface ButtonGroupProps {
     group: string,
-    labels: string[],
+    labels: Label,
     type?: string,
 }
 
+export const ButtonGroupContext = createContext<ButtonGroupType>(null!)
+
 export function ButtonGroup({ group, labels, type="single" }: ButtonGroupProps) {
-    const groupContext = createContext<number>(null!)
     const [buttonState, setButtonState] = useState<number>(null!)
 
+    const labelToButton = (label: Label) => {
+        if (typeof label == "string")
+            return <SelectionButton group={group} text={label} />
+        return <SelectionButton group={group} label={label.label} text={label.text} />
+    }
+
     return (
-        <groupContext.Provider value={{ buttonState, setButtonState }}>
-            {labels.map(label => <SelectionButton group={group} label={label} />)}
-        </groupContext.Provider>
+        <ButtonGroupContext.Provider value={{ buttonState, setButtonState }}>
+            {labels.map(labelToButton)}
+        </ButtonGroupContext.Provider>
     )
 }
--- frontend/src/components/buttons/SelectionButton.tsx
+++ frontend/src/components/buttons/SelectionButton.tsx
@@ -1,12 +1,18 @@
+import { ButtonGroupContext, ButtonGroupType } from "./ButtonGroup"
+
 interface SelectionProps {
     group: string,
     text: string,
+    label?: string,
 }
 
-export function SelectionButton({ group, text }: SelectionProps) {
+export function SelectionButton({ group, text, label }: SelectionProps) {
     return (
-        <button>
-            {text}
-        </button>
+        <label>
+            {label}
+            <button>
+                {text}
+            </button>
+        </label>
     )
 }
