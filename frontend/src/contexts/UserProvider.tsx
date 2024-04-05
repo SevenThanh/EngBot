6e83453488c9d51290b4887e7dc1d16091559c7f
Kevin Cheng <kev72eat@gmail.com>
2024-04-05 00:46:13 -0400

    seed userinfo context manager with mock default user value

--- frontend/src/contexts/UserProvider.tsx
+++ frontend/src/contexts/UserProvider.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react"
+import { createContext, useState, useEffect } from "react"
 import { User } from "@/types"
 
 export type UserContextType = {
@@ -6,10 +6,40 @@ export type UserContextType = {
     setUserInfo: (user: User) => void
 }
 
+async function MOCK_USER() {
+    return {
+        id: "kevinthuhstink",
+        username: "kevinthuhstink",
+        type: "student",
+        proficiency: 0
+    }
+}
+
+const NULL_USER: User = {
+    id: "null",
+    username: "null",
+    type: "null",
+    proficiency: -1
+}
+
 export const UserContext = createContext<UserContextType>(null!)
 
 export function UserProvider({ children }: React.PropsWithChildren) {
     const [userInfo, setUserInfo] = useState<User>(null!)
+
+    useEffect(() => {
+        async function fetchUserData() {
+            //will replace with actual user data fetching eventually
+            return await MOCK_USER()
+        }
+
+        fetchUserData()
+            .then((res: User) => setUserInfo(res))
+            .catch(error => {
+                console.log((error as Error).message)
+                setUserInfo(NULL_USER)
+            })
+    }, [])
 
     return (
         <UserContext.Provider value={{ userInfo, setUserInfo }}>
--- frontend/src/types/index.tsx
+++ frontend/src/types/index.tsx
@@ -1,5 +1,6 @@
 export type User = {
     id?: string, //optional FOR NOW
+    username: string, //password will be handled with firebase authentication
     type: string, //student, parent, etc.
     proficiency: number, //english skill elo
 }
