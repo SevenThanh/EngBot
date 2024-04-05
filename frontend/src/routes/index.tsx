51f2db989e6b85d8b6600faf5d867726e59a3041
Kevin Cheng <kev72eat@gmail.com>
2024-04-05 18:01:42 -0400

    change routes to always allow user home page (will change in the future)

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,16 +1,10 @@
-import { useContext } from "react"
 import { Routes, Route } from 'react-router-dom'
 import { UserRegistration, Title, Login, Home } from "@/pages"
-import { UserContext } from "@/contexts"
 
 export default function AppRoutes() {
-    const { userInfo } = useContext(UserContext)
-
     return (
         <Routes>
-            { userInfo && userInfo.id !== "null" &&
-                <Route path="home" element={<Home />} />
-            }
+            <Route path="home" element={<Home />} />
             <Route path="get-started" element={<UserRegistration />} />
             <Route path="login" element={<Login />} />
             <Route path="*" element={<Title />} />
