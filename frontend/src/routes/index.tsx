e47eb688c2701fb5bce32c3acb7adc3327c2c547
Kevin Cheng <kev72eat@gmail.com>
2024-04-05 00:50:13 -0400

    init user home file

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,10 +1,17 @@
+import { useContext } from "react"
 import { Routes, Route } from 'react-router-dom'
-import { UserRegistration, Title, Login } from "@/pages"
+import { UserRegistration, Title, Login, Home } from "@/pages"
+import { UserContext } from "@/contexts"
 
 export default function AppRoutes() {
+    const { userInfo } = useContext(UserContext)
+
     return (
         <Routes>
-            <Route path="/get-started" element={<UserRegistration />} />
+            { userInfo.id !== null &&
+                <Route path="home" element={<Home />} />
+            }
+            <Route path="get-started" element={<UserRegistration />} />
             <Route path="login" element={<Login />} />
             <Route path="*" element={<Title />} />
         </Routes>
