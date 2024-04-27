2fb719a9bfa767c667641c6123fbd779595ce405
Kevin Cheng <kev72eat@gmail.com>
2024-04-26 21:28:40 -0400

    remove UserContext from AuthContext, if possible handle user auth and user properties separately

--- frontend/src/contexts/AuthProvider.tsx
+++ frontend/src/contexts/AuthProvider.tsx
@@ -1,22 +1,21 @@
-import { createContext, useState, useEffect, useContext } from "react"
-import { UserContext } from "./UserProvider"
+import { createContext, useState, useEffect } from "react"
 import { LOADING_STATUS, UserAuth } from "@/types"
 import { MOCK_AUTH } from "@/lib/mock_data"
 
-export const AuthContext = createContext(null!)
+export const AuthContext = createContext<UserAuth>(null!)
 
 export function AuthProvider({ children }: React.PropsWithChildren) {
-    const { userInfo, setUserInfo } = useContext(UserContext)
     const [userAuth, setUserAuth] = useState<UserAuth>(null!)
     const [status, setStatus] = useState<LOADING_STATUS>(1)
 
     useEffect(() => {
-        MOCK_AUTH().then(res => {
+        async function fetchAuth() {
+            //will replace with actual user data fetching eventually
+            return await MOCK_AUTH()
+        }
+
+        fetchAuth().then(res => {
             setUserAuth(res)
-            setUserInfo({
-                ...userInfo,
-                ...res
-            })
             setStatus(0)
 
         }).catch(error => {
--- frontend/src/contexts/Providers.tsx
+++ frontend/src/contexts/Providers.tsx
@@ -5,7 +5,7 @@ import { UserProvider } from "./UserProvider"
 import { AuthProvider } from "./AuthProvider"
 
 //UPDATE THIS VAR WITH ANY NEW CONTEXT PROVIDERS
-const providers = [ AuthProvider, UserProvider]
+const providers = [ AuthProvider, UserProvider ]
 
 export function Providers({ children }: React.PropsWithChildren) {
     return (
