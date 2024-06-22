cdb7348ff9208232962ebf8d7c2419656aa43cd7
Johan Nguyen <thaithanhace@gmail.com>
2024-06-21 19:22:34 -0500

    Added english level questions

--- frontend/src/pages/EngLevel.tsx
+++ frontend/src/pages/EngLevel.tsx
@@ -3,24 +3,22 @@ import { useNavigate } from 'react-router-dom';
 
 
 
-const userTypes = ['Student', 'Teacher', 'Parent', 'Professional', 'Other'];
-const UserIdentity: React.FC = () => {
-    const navigate = useNavigate();
-    const handleButtonClick = (userType: string) => {
-        console.log('Selected:', userType);
-        navigate('/englevel');
+const englishLevel = ['New', 'Beginner', 'Intermediate', 'Advance'];
+const EngLevel: React.FC = () => {
+    const handleButtonClick = (englishLevel: string) => {
+        console.log('Selected:', englishLevel);
     };
 
     return (
         <div className="w-screen h-screen bg-gradient-to-r from-light_100 flex items-center justify-center px-10">
             <img src="/student.png" alt="Robot" className="w-1/6 h-auto mr-80" />
             <div className="w-1/3 bg-menu-blue p-10 rounded-lg shadow-xl">
-                <h1 className="text-3xl font-bold mb-6 text-center">Which best describes you?</h1>
+                <h1 className="text-3xl font-bold mb-6 text-center">Select your English level</h1>
                 <div className="flex flex-col space-y-4">
-                    {userTypes.map(type => (
+                    {englishLevel.map(type => (
                         <button 
                             key={type}
-                            className="w-full p-4 text-xl text-white bg-options-blue rounded hover:bg-blue-800"
+                            className="w-full p-4 text-xl text-white bg-options-blue"
                             onClick={() => handleButtonClick(type.toLowerCase())}>
                             {type}
                         </button>
@@ -31,4 +29,4 @@ const UserIdentity: React.FC = () => {
     );
 };
 
-export default UserIdentity;
+export default EngLevel;
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,13 +2,15 @@ import { Routes, Route } from 'react-router-dom'
 import { Home } from "@/pages/Home"
 import { Login } from "@/pages/Login"
 import UserIdentity from "@/pages/UserIdentity";
+import EngLevel from "@/pages/EngLevel"
 
 export default function AppRoutes() {
     return (
         <Routes>
             <Route path="login" element={<Login />} />
             <Route path="*" element={<Home />} />
-            <Route path="/identity" element={<UserIdentity />} />
+            <Route path="identity" element={<UserIdentity />} />
+            <Route path="englevel" element={<EngLevel />} />
         </Routes>
     )
 }
