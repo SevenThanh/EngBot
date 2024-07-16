1abcd8b9a052ebc3eaeaa01008978f6e7bf4aaaf
Kevin Cheng <kev72eat@gmail.com>
2024-07-16 19:10:06 -0400

    specify user registration question with dynamic route variable rather than useState

--- frontend/src/pages/UserRegistration.tsx
+++ frontend/src/pages/UserRegistration.tsx
@@ -1,129 +1,60 @@
-import { useState, useContext } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useContext } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
 import { UserContext } from '@/contexts'
-import { Layout, Question } from "@/layouts/user_registration"
-
-interface QuestionFormat {
-    question: string,
-    labels: string[],
-    group?: string,
-    type?: string,
-    key: number
-}
-
-const questions = [
-    {
-        question: "Which best describes you?",
-        group: 'type',
-        labels: [
-            "Student",
-            "Teacher",
-            "Parent",
-            "Professional",
-            "Other"
-        ],
-        key: 0
-    },
-    {
-        question: "Select your English proficiency.",
-        group: 'proficiency',
-        labels: [
-            "New",
-            "Beginner",
-            "Intermediate",
-            "Advanced"
-        ],
-        key: 1
-    },
-    {
-        question: "Why do you want to learn English?",
-        small: "select all that apply",
-        type: "multiselect",
-        group: "learning_reason",
-        labels: [
-            "Education",
-            "Connect with others",
-            "Travel",
-            "For fun",
-            "Other"
-        ],
-        key: 2
-    },
-    {
-        question: "How did you hear about us?",
-        group: "heard_from",
-        labels: [
-            "TikTok",
-            "YouTube",
-            "Instagram",
-            "Google Search",
-            "Other"
-        ],
-        key: 3
-    }
-]
-const questionsCount = questions.length
+import { Question } from "@/layouts/user_registration"
+import { questions } from "@/layouts/user_registration/questions"
 
 export function UserRegistration() {
-    const [questionNum, setQuestionNum] = useState<number>(0);
     const { userInfo, setUserInfo } = useContext(UserContext)
+    const questionId = parseInt(useParams().questionId as string)
+    const currentQuestion = questions[questionId]
     const navigate = useNavigate()
 
-    function submitQuestion(active: number[]) {
-        if (active.length === 0)
+    function submitQuestion(selected: number[]) {
+        if (selected.length === 0)
             return
 
-        const currentQuestion: QuestionFormat = questions[questionNum]
+        if (!currentQuestion.type || currentQuestion.type === "single")
+            setUserInfo({
+                ...userInfo,
+                [currentQuestion.group]: currentQuestion.labels[selected[0]]
+            })
 
-        if (currentQuestion.group) {
-            //single selections
-            if (!currentQuestion.type || currentQuestion.type === "single")
-                setUserInfo({
-                    ...userInfo,
-                    [currentQuestion.group]: currentQuestion.labels[active[0]]
-                })
+        else //multiple selections
+            setUserInfo({
+                ...userInfo,
+                [currentQuestion.group]:
+                selected.map(index => currentQuestion.labels[index])
+            })
 
-            else //multiple selections
-                setUserInfo({
-                    ...userInfo,
-                    [currentQuestion.group]:
-                        active.map(index => currentQuestion.labels[index])
-                })
-        }
-
-        if (questionNum == questionsCount - 1) {
+        if (questionId == questions.length - 1) {
             console.log(userInfo)
             navigate('/home')
             return
         }
 
-        setQuestionNum(prevNum => prevNum + 1)
+        navigate(`/get-started/${questionId + 1}`)
     }
 
-    const questionElements = questions.map(
-        (elem: QuestionFormat) => {
-            const buttonShape = "px-4 py-2 my-2 w-full rounded-xl text-center "
-            return <Question
-                {...elem}
+
+    return (
+        <>
+            <Question
+                {...currentQuestion}
                 onSubmit={submitQuestion}
                 style={{
                     wrapperStyle: "flex flex-col items-center my-8 md:w-[70%] w-max-[400px]",
-                    submitStyle: buttonShape + "bg-engbot-700 text-neutral-300",
-                    buttonStyle: buttonShape + "bg-engbot-100 text-engbot-700",
-                    selectedStyle: buttonShape + "bg-sky-600 text-neutral-100"
+                    submitStyle: "px-4 py-2 my-2 w-full rounded-xl text-center bg-engbot-700 text-neutral-300",
+                    buttonStyle: "px-4 py-2 my-2 w-full rounded-xl text-center bg-engbot-100 text-engbot-700",
+                    selectedStyle: "px-4 py-2 my-2 w-full rounded-xl text-center bg-sky-600 text-neutral-100"
                 }}
             />
-        }
-    )
 
-    return (
-        <Layout>
-            {questionElements[questionNum]}
-            { questionNum !== 0 &&
-                <p onClick={() => setQuestionNum(prevNum => prevNum - 1)} className="cursor-pointer">
+            { questionId !== 0 &&
+                <p onClick={() => navigate(`/get-started/${questionId - 1}`)} className="cursor-pointer">
                     Go back
                 </p>
             }
-        </Layout>
+        </>
     )
 }
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,11 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
+import { Layout as UserRegistrationLayout } from "@/layouts/user_registration"
 import { UserRegistration, Title, Login, Home } from "@/pages"
 
 export default function AppRoutes() {
     return (
         <Routes>
             <Route path="home" element={<Home />} />
-            <Route path="get-started" element={<UserRegistration />} />
+            <Route path="get-started" element={<UserRegistrationLayout />}>
+                <Route
+                    path=":questionId"
+                    element={<UserRegistration />}
+                />
+            </Route>
             <Route path="login" element={<Login />} />
             <Route path="*" element={<Title />} />
         </Routes>
